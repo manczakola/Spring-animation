3a63cc48560007df6ed62291d95e615074eb53f1
Ola Manczak <manczakola@gmail.com>
2021-01-07 11:30:13 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const fill = document.querySelector('.fill');
+const button = document.querySelector('.btn-action');
+const ball = document.querySelector('.ball');
+const spring = document.querySelector('.spring');
+const bar = document.querySelector('.bar');
+
+
+
+
+
+const stretchSpring = () => {
+    fill.style.animationName = 'fill';
+    fill.style.animationPlayState = 'running';
+    spring.style.animationPlayState = 'running';
+    button.textContent = "Release the spring";
+
+    button.removeEventListener('mousedown', stretchSpring);
+    button.removeEventListener('touchstart', stretchSpring);
+
+};
+
+const releaseSpring = () => {
+
+    fill.style.animationPlayState = 'paused';
+    button.textContent = "Stretch the spring";
+    const fillStyles = getComputedStyle(fill);
+
+    const fillWidth = parseInt(fillStyles.width, 10);
+    const barWidth = parseInt(getComputedStyle(bar).width);
+    const power = parseInt(((fillWidth / barWidth) * 100).toFixed());
+    button.textContent = `Power ${power}%`;
+
+    button.removeEventListener('mousedown', stretchSpring);
+    button.removeEventListener('touchstart', stretchSpring);
+
+    document.documentElement.style.setProperty("--power-x", `${power+20}%`); //HTML style
+    ball.style.animation = 'fly-ball-x 1s 1 forwards .15s cubic-bezier(.17, .67, .6, 1), fly-ball-y 1s 1 forwards .15s linear';
+
+    document.documentElement.style.setProperty("--spring-left", getComputedStyle(spring).left);
+    spring.style.animation = 'release-spring .2s 1 forwards linear';
+
+    // blocking click
+
+    button.removeEventListener('mouseup', stretchSpring);
+    button.removeEventListener('touchend', stretchSpring);
+
+    ball.addEventListener('animationend', resetAnimation);
+
+};
+
+const resetAnimation = () => {
+
+    ball.removeEventListener('animationend', resetAnimation);
+
+    setTimeout(() => {
+        button.addEventListener('mousedown', stretchSpring);
+        button.addEventListener('mouseup', releaseSpring);
+        button.addEventListener('touchstart', stretchSpring);
+        button.addEventListener('touchend', releaseSpring);
+
+
+        button.textContent = 'Stretch the spring';
+
+        spring.style.animation = '';
+        ball.style.animation = '';
+        fill.style.animationName = 'none'
+
+    }, 2000)
+
+
+
+};
+
+
+button.addEventListener('mousedown', stretchSpring);
+button.addEventListener('mouseup', releaseSpring);
+button.addEventListener('touchstart', stretchSpring);
+button.addEventListener('touchend', releaseSpring);
